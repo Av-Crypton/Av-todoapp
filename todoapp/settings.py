dddf2386edcd24911f9212786b72c075c4eb52e6
Av-Crypton <executivedev22@gmail.com>
2022-03-20 19:32:30 +0500

    Heroku settings updated

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t*srlf==uqq1h@djrp4%k3$kc=^j36&a1=(&8$^tpgk+huuo_a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['av-todoapp.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -44,6 +44,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
